d8ada5d326b6ddb6fce88e67e7df5d15bb36fa25
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2025-03-04 16:41:25 +0100

    Redering content based on button click / user profile section

--- client/app.js
+++ client/app.js
@@ -1,4 +1,3 @@
-// Changing content based on button click
 const navLinks = document.querySelectorAll(".navigation a");
 const mainDashboard = document.querySelector(".main_dashboard");
 
@@ -12,6 +11,9 @@ function loadContent(section, addToHistory = true) {
     })
     .then((html) => {
       mainDashboard.innerHTML = html;
+
+      updateActiveLink(section);
+
       if (addToHistory) {
         history.pushState({ section }, "", `?page=${section}`);
       }
@@ -29,18 +31,40 @@ navLinks.forEach((link) => {
   });
 });
 
-async function fetchUsers() {
-  try {
-    const response = await fetch("http://localhost:8081/recipes");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+function updateActiveLink(activeSection) {
+  navLinks.forEach((link) => {
+    if (link.getAttribute("data-section") === activeSection) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
     }
-    const recipes = await response.json();
-
-    console.log(recipes);
-  } catch (error) {
-    console.error("Error fetching recipes:", error);
-  }
+  });
 }
 
-fetchUsers();
+window.addEventListener("popstate", (event) => {
+  if (event.state && event.state.section) {
+    loadContent(event.state.section, false);
+  }
+});
+
+async function loadStartPage() {
+  loadContent("start", false);
+}
+
+window.addEventListener("DOMContentLoaded", loadStartPage);
+
+// async function fetchUsers() {
+//   try {
+//     const response = await fetch("http://localhost:8081/recipes");
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     const recipes = await response.json();
+
+//     console.log(recipes);
+//   } catch (error) {
+//     console.error("Error fetching recipes:", error);
+//   }
+// }
+
+// fetchUsers();
